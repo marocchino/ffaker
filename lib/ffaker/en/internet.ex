da118cd6357517c481469ba9f914f010cdafeeec
Shim Won <marocchino@gmail.com>
2016-10-21 11:49:23 +0900

    Add en internet

---
+++ lib/ffaker/en/internet.ex
@@ -0,0 +1,191 @@
+defmodule Ffaker.En.Internet do
+  import Ffaker, only: [random: 1]
+  alias Ffaker.En.Name
+  alias Ffaker.En.Lorem
+
+  @moduledoc """
+  Functions for internet data in English
+  """
+  @hosts ~w(gmail.com yahoo.com hotmail.com)
+  @free_hosts ~w(gmail.com yahoo.com hotmail.com)
+  @disposable_hosts ~w(mailinator.com
+                       suremail.info
+                       spamherelots.com
+                       binkmail.com
+                       safetymail.info)
+  @domain_suffixes ~w(co.uk com us ca biz info name)
+  @safe_domain_suffixes ~w(org com net)
+  @slug_delimiters ~w(- _ .)
+
+  @doc """
+  Returns internet
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.email
+      "dustin.oberbrunner@gmail.com"
+  """
+  @spec email() :: String.t
+  def email do
+    "#{user_name}@#{@hosts |> random}"
+  end
+
+  @doc """
+  Returns internet
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.disposable_email
+      "dustin.oberbrunner@mailinator.com"
+  """
+  @spec disposable_email() :: String.t
+  def disposable_email do
+    "#{user_name}@#{@disposable_hosts |> random}"
+  end
+
+  @doc """
+  Returns internet
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.disposable_email
+      "dustin.oberbrunner@gmail.com"
+  """
+  @spec free_email() :: String.t
+  def free_email do
+    "#{user_name}@#{@free_hosts |> random}"
+  end
+
+  @doc """
+  Returns internet
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.disposable_email
+      "dustin.oberbrunner@example.com"
+  """
+  @spec safe_email() :: String.t
+  def safe_email do
+    "#{user_name}@example.#{@safe_domain_suffixes |> random}"
+  end
+
+  @doc """
+  Returns user_name
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.user_name
+      "dustin.oberbrunner"
+  """
+  @spec user_name() :: String.t
+  def user_name do
+    Name.name
+    |> escape
+  end
+
+  @doc """
+  Returns domain_name
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.domain_name
+      "oberbrunner.com"
+  """
+  @spec domain_name() :: String.t
+  def domain_name do
+    "#{Name.last_name |> escape}#{@domain_suffixes |> random}"
+  end
+
+  @doc """
+  Returns uri
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.uri
+      "https://oberbrunner.com"
+  """
+  @spec uri(String.t) :: String.t
+  def uri(protocol \\ "https") do
+    "#{protocol}://#{domain_name}"
+  end
+
+  @doc """
+  Returns ip_v4_address
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.ip_v4_address
+      "192.168.0.1"
+  """
+  @spec ip_v4_address() :: String.t
+  def ip_v4_address do
+    [0..255 |> random,
+     0..255 |> random,
+     0..255 |> random,
+     0..255 |> random]
+    |> Enum.join(".")
+  end
+
+  @doc """
+  Returns slug
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.slug
+      "quia-ipsa"
+  """
+  @spec slug(Enum.t, String.t) :: String.t
+  def slug(words \\ nil, glue \\ nil) do
+    words = words ||
+            2..2
+            |> Lorem.sentence
+            |> String.trim(".")
+    glue = glue || @slug_delimiters |> random
+
+    words
+    |> String.downcase
+    |> String.replace(~r/\W/, glue)
+  end
+
+  @doc """
+  Returns password
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.password
+      "aysqkeiksw"
+  """
+  @spec password(Range.t) :: String.t
+  def password(length_range \\ 8..16) do
+    length = length_range |> random
+    1..length
+    |> Enum.map(fn _ -> ?a..?z |> random end)
+    |> to_string
+
+  end
+
+  @doc """
+  Returns mac
+
+  ## Examples
+
+      iex> Ffaker.En.Internet.mac
+      "DC:4B:61:EB:30:8A"
+  """
+  @spec mac() :: String.t
+  def mac do
+    1..281_474_976_710_655
+    |> random
+    |> Integer.to_string(16)
+    |> String.pad_leading(12, "0")
+    |> String.replace(~r(..), "\\0:")
+    |> String.trim(":")
+  end
+
+  @spec escape(String.t) :: String.t
+  defp escape(str) do
+    str
+    |> String.downcase
+    |> String.replace(~r([^a-z]), ".")
+  end
+end
---
+++ test/ffaker/en/internet_test.exs
@@ -0,0 +1,91 @@
+defmodule Ffaker.En.InternetTest do
+  use ExUnit.Case, async: true
+  import Ffaker.En.Internet
+  @free_hosts ~w(gmail.com yahoo.com hotmail.com)
+  @disposable_hosts ~w(mailinator.com
+                       suremail.info
+                       spamherelots.com
+                       binkmail.com
+                       safetymail.info)
+
+  test "email/0" do
+    assert Regex.match?(~r/\A[a-z.]+@[a-z.]+\z/, email),
+          "#{email} is not matched"
+  end
+
+  test "disposable_email/0" do
+    assert host(disposable_email) in @disposable_hosts
+  end
+
+  test "free_email/0" do
+    assert host(free_email) in @free_hosts
+  end
+
+  test "safe_email/0" do
+    actual = safe_email
+    assert Regex.match?(~r/\A[a-z.]+@example\.(org|com|net)\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "user_name/0" do
+    actual = user_name
+    assert Regex.match?(~r/\A[a-z.]+\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "domain_name/0" do
+    actual = domain_name
+    assert Regex.match?(~r/\A[a-z.]+\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "uri/1" do
+    actual = uri("ftp")
+    assert Regex.match?(~r/\Aftp:\/\/[a-z.]+\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "uri/0" do
+    actual = uri
+    assert Regex.match?(~r/\Ahttps:\/\/[a-z.]+\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "ip_v4_address/0" do
+    actual = ip_v4_address
+    assert Regex.match?(~r/\A(?:\d{1,3}\.){3}\d{1,3}\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "slug/2" do
+    assert slug("a b", ":") == "a:b"
+  end
+
+  test "slug/0" do
+    assert slug == "a:b"
+  end
+
+  test "password/1" do
+    actual = password(10..10)
+    assert Regex.match?(~r/\A[a-z]{10}\z/, actual),
+          "#{actual} is not matched"
+  end
+  test "password/0" do
+    actual = password
+    assert Regex.match?(~r/\A[a-z]{8,16}\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  test "mac/0" do
+    actual = mac
+    assert Regex.match?(~r/\A(?:[A-F0-9]{2}:){5}[A-F0-9]{2}\z/, actual),
+          "#{actual} is not matched"
+  end
+
+  @spec host(String.t) :: String.t
+  defp host(email) do
+    email
+    |> String.split("@")
+    |> Enum.at(1)
+  end
+end
